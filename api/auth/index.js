11a57401fd23e04af9007ef1c6016d1fc679dc1f
Ahmed Adel <w33zy.7@gmail.com>
2019-03-09 13:34:39 +0200

    🐛 Add default JWT secret code

--- api/auth/index.js
+++ api/auth/index.js
@@ -1,6 +1,6 @@
 require('dotenv').config()
 const jwt = require('jsonwebtoken');
-const JWT_SECRET = process.env.JWT_SECRET;
+const JWT_SECRET = process.env.JWT_SECRET || 'secretcode';
 
 module.exports.generateJWT = function (id) {
     // encoded object
