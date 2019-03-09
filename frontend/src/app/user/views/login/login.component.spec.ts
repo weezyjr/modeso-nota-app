bda7b6b07c83493c9a61624236046a11b7fe7de7
Ahmed Adel <w33zy.7@gmail.com>
2019-03-09 13:30:56 +0200

    ⭐ Add Login form

--- frontend/src/app/user/views/login/login.component.spec.ts
+++ frontend/src/app/user/views/login/login.component.spec.ts
@@ -1,6 +1,8 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { LoginComponent } from './login.component';
+import { HttpClientModule } from '@angular/common/http';
+import { NotifierModule } from 'angular-notifier';
 
 describe('LoginComponent', () => {
   let component: LoginComponent;
@@ -8,6 +10,10 @@ describe('LoginComponent', () => {
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
+      imports: [
+        HttpClientModule,
+        NotifierModule
+      ],
       declarations: [ LoginComponent ]
     })
     .compileComponents();
--- frontend/src/app/user/views/login/login.component.ts
+++ frontend/src/app/user/views/login/login.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { AuthService } from 'src/app/auth/auth.service';
+import { User } from 'src/app/models/User';
+import { NotifierService } from 'angular-notifier';
 
 @Component({
   selector: 'app-login',
@@ -7,9 +10,23 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LoginComponent implements OnInit {
 
-  constructor() { }
+  constructor(private authService: AuthService, private notifierService: NotifierService) { }
 
-  ngOnInit() {
+  // create user object
+  public user: User = new User();
+
+  async login(formIsValid: boolean) {
+    // check if the form is valid
+    if (formIsValid) {
+      // login
+      await this.authService.login(this.user.username, this.user.password)
+        .subscribe((msg) => {
+          this.notifierService.notify('success', msg);
+          // TODO: Move to another page
+        });
+    }
   }
+
+  ngOnInit() {}
 
 }
