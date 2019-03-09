a3933bb37440b676461589a0a95cd152cbb9718a
Ahmed Adel <w33zy.7@gmail.com>
2019-03-10 00:02:09 +0200

    🚧 Create Note model class

---
+++ frontend/src/app/models/Note.ts
@@ -0,0 +1,9 @@
+export class Note {
+  public id: string;
+  public title: string;
+  public text: string;
+  public image: string;
+  public imageFile: string;
+  public author: string;
+  public public: boolean;
+}
