4563294e05fb0d2d1ab30f8a9cccb2efbf064a9f
Ahmed Adel <w33zy.7@gmail.com>
2019-03-10 01:00:19 +0200

    ⭐ Add my-notes page to routing module
    `/note`

--- frontend/src/app/app-routing.module.ts
+++ frontend/src/app/app-routing.module.ts
@@ -2,12 +2,13 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { LoginComponent } from './user/views/login/login.component';
 import { RegisterComponent } from './user/views/register/register.component';
+import { MyNotesPageComponent } from './notes/my-notes-page/my-notes-page.component';
 
 const routes: Routes = [
   { path: '', component: LoginComponent },
   { path: 'login', component: LoginComponent },
-  { path: 'register', component: RegisterComponent }
-
+  { path: 'register', component: RegisterComponent },
+  { path: 'notes', component: MyNotesPageComponent }
 ];
 
 @NgModule({
