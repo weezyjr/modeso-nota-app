d43669d829986c7faa979a55b3250fe0c1d9fa39
Ahmed Adel <w33zy.7@gmail.com>
2019-03-11 19:28:55 +0200

    🎨 Create modal component

---
+++ frontend/src/app/ui/modal/modal.component.ts
@@ -0,0 +1,16 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-modal',
+  templateUrl: './modal.component.html',
+  styleUrls: ['./modal.component.scss']
+})
+export class ModalComponent implements OnInit {
+
+  // TODO: close modal on `esc` keyboard event and click outside the popup
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
