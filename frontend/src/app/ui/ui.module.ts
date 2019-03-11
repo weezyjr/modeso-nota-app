19f5d225b90a9e475195a091f39138ff9d6f243c
Ahmed Adel <w33zy.7@gmail.com>
2019-03-11 19:29:25 +0200

    🔧 Update UI module
    include and export the new components (modal, note-form ..

--- frontend/src/app/ui/ui.module.ts
+++ frontend/src/app/ui/ui.module.ts
@@ -1,11 +1,23 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { NoteComponent } from './note/note.component';
+import { NoteFormComponent } from './note-form/note-form.component';
+import { SidebarComponent } from './sidebar/sidebar.component';
+import { AppRoutingModule } from '../app-routing.module';
+import { FormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
+import { ModalComponent } from './modal/modal.component';
+import { NotifierModule } from 'angular-notifier';
+
 
 @NgModule({
-  declarations: [NoteComponent],
+  declarations: [NoteComponent, NoteFormComponent, SidebarComponent, ModalComponent],
   imports: [
-    CommonModule
-  ], exports: [NoteComponent]
+    CommonModule,
+    HttpClientModule,
+    FormsModule,
+    AppRoutingModule,
+    NotifierModule,
+  ], exports: [NoteComponent, NoteFormComponent, SidebarComponent, ModalComponent]
 })
 export class UiModule { }
