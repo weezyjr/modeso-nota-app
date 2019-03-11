161ee15744ee35286385cc46601a5e29ab397ae0
Ahmed Adel <w33zy.7@gmail.com>
2019-03-12 00:32:45 +0200

    🐛 Fix: modal exit on background click

--- frontend/src/app/ui/modal/modal.component.ts
+++ frontend/src/app/ui/modal/modal.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Output, HostListener, EventEmitter } from '@angular/core';
 
 @Component({
   selector: 'app-modal',
@@ -7,7 +7,21 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ModalComponent implements OnInit {
 
-  // TODO: close modal on `esc` keyboard event and click outside the popup
+  @Output() clickOutside: EventEmitter<boolean> = new EventEmitter();
+
+  // keyboard escape key
+  @HostListener('document:keydown.escape', ['$event']) onKeydownHandler(event: KeyboardEvent) {
+    this.clickOutside.emit(true);
+  }
+
+  // (evenet: { ... }) just to make the linter happy :)
+  handleOutsideClick(event: { target: { classList: string[]; }; }) {
+    // check if it is the background
+    if (event.target.classList && event.target.classList[0] === 'modal') {
+      this.clickOutside.emit(true);
+    }
+  }
+
   constructor() { }
 
   ngOnInit() {
